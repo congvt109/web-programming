f4da69843afd8c893ff1e671e46b86b9937d2201
congvt109 <congvubk109@gmail.com>
2022-11-26 12:10:55 +0700

    feat(#7): done lab7-5

--- lab07/7-5/index.php
+++ lab07/7-5/index.php
@@ -9,17 +9,25 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   <style>
-    .form-control {
-      width: 500px;
-    }
-    label, input, button{
-      margin-left: 10px;
-    }
+  body {
+    font-family: sans-serif;
+  }
+
+  .form-control {
+    width: 500px;
+  }
+
+  label,
+  input,
+  button,
+  h5 {
+    margin-left: 10px;
+  }
   </style>
 </head>
 
 <body>
-  <form action="process.php" method="POST">
+  <form action="#" method="POST">
     <div class="form-group">
       <label for="email">Email address</label>
       <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
@@ -41,6 +49,30 @@
     <button type="submit" class="btn btn-primary">Submit</button>
     <button type="reset" class="btn btn-primary">Reset</button>
   </form>
+
+  <?php
+  $email = $_POST["email"];
+  $url_addr = $_POST["url_addr"];
+  $phone_number = $_POST["phone_number"];
+  $bool = true;
+  echo "<br>";
+  if (isset($email) && !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $bool = false;
+    echo "<h5>$email is invalid email</h5>";
+  }
+  if (isset($url_addr) && !preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $url_addr)) {
+    $bool = false;
+    echo "<h5>$url_addr is invalid URL address</h5>";
+  }
+  if (isset($phone_number) && !preg_match('/^[0-9]{10}+$/', $phone_number)) {
+    $bool = false;
+    echo "<h5>$phone_number is invalid phone number</h5>";
+  }
+  if ($bool) {
+    echo "<h5> All your information is validate</h5>";
+  }
+  ?>
+
 </body>
 
 </html>
--- lab07/7-5/process.php
+++ lab07/7-5/process.php
@@ -6,6 +6,11 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>CheckDateTime</title>
+  <style>
+  body {
+    font-family: sans-serif;
+  }
+  </style>
 </head>
 
 <body>
@@ -16,13 +21,13 @@
 
   if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
     echo "<h3>$email is invalid email</h3>";
-  } 
-  if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$url_addr)) {
+  } else if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $url_addr)) {
     echo "<h3>$url_addr is invalid URL address</h3>";
-  }
-  if(!preg_match('/^[0-9]{10}+$/', $phone)) {
+  } else if (!preg_match('/^[0-9]{10}+$/', $phone_number)) {
     echo "<h3>$phone_number is invalid phone number</h3>";
-    }
+  } else {
+    echo "<h3> All your information is validate</h3>";
+  }
   ?>
 </body>
 
