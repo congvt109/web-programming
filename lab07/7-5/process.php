1d1a818633af4e246c246580f6ac553eb7c59700
congvt109 <congvubk109@gmail.com>
2022-11-26 11:52:38 +0700

    feat(#6): lab7-5 of lab07

---
+++ lab07/7-5/process.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CheckDateTime</title>
+</head>
+
+<body>
+  <?php
+  $email = $_POST["email"];
+  $url_addr = $_POST["url_addr"];
+  $phone_number = $_POST["phone_number"];
+
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo "<h3>$email is invalid email</h3>";
+  } 
+  if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$url_addr)) {
+    echo "<h3>$url_addr is invalid URL address</h3>";
+  }
+  if(!preg_match('/^[0-9]{10}+$/', $phone)) {
+    echo "<h3>$phone_number is invalid phone number</h3>";
+    }
+  ?>
+</body>
+
+</html>
